313870174c1547663abc32a14e2165d2ac4177b4
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2023-11-07 13:58:30 +0100

    Fix config

--- rl/algorithms/learning_algorithms.py
+++ rl/algorithms/learning_algorithms.py
@@ -26,43 +26,38 @@ class DQN(LearningAlgorithm):
         super().__init__(logger)
         
         # Size of freecell input after it's flattened
+        # TO DO - change how it's used
         self.n_observations = 2720
 
         # Number of possible moves (some of them may be illegal)
         # TO DO - get all possible moves
         self.n_actions = 600
 
-        # Epsilon values
-        self.eps_start = 0.9
-        self.eps_end = 0.05
-        self.eps_decay = 1000
-
         # self.gamma = 0.99
         # TAU = 0.005
         # LR = 1e-4
-
-        # Extra params
-        self.use_gpu = True
-        self.seed = 1001
+        # BATCH_SIZE = 128
 
         # Unmodifiable params
         self.steps_done = 0
-        self.device = torch.device("cuda" if torch.cuda.is_available() and self.use_gpu else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() and self.config.use_gpu else "cpu")
 
         # Model setup - uncomment later seeds
-        #random.seed(self.seed)
-        #torch.manual_seed(self.seed)
+        #random.seed(self.config.seed)
+        #torch.manual_seed(self.config.seed)
         self.policy_net = SimpleNet([self.n_observations, 20, 30]).to(self.device)
         self.target_net = SimpleNet([self.n_observations, 20, 30]).to(self.device)
         self.target_net.load_state_dict(self.policy_net.state_dict())
+
+        self.state = None
 
     # TO DO - decide how to get the list of all possible moves
     def make_action(self, state: list, actions: list[list]) -> list:
         state = torch.tensor(state, dtype=torch.float32, device=self.device).unsqueeze(0)
 
         sample = random.random()
-        eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * \
-            math.exp(-1. * self.steps_done / self.eps_decay)
+        eps_threshold = self.config.eps_end + (self.config.eps_start - self.config.eps_end) * \
+            math.exp(-1. * self.steps_done / self.config.eps_decay)
         self.steps_done += 1
 
         if sample > eps_threshold:
@@ -78,7 +73,11 @@ class DQN(LearningAlgorithm):
 
     @classmethod
     def _get_train_params(cls) -> dict:
-        return {"n_observations": (ParameterType.INT.name, 2720, 1, 20000)}
+        return {"eps_start": (ParameterType.FLOAT.name, 0.9, 0, 10),
+                "eps_end": (ParameterType.FLOAT.name, 0.05, 0, 10),
+                "eps_decay": (ParameterType.FLOAT.name, 1000, 0, 10000),
+                "use_gpu": (ParameterType.BOOL.name, False, None, None),
+                "seed": (ParameterType.INT.name, 1001, 0, 100000)}
 
     @classmethod
     def _get_test_params(cls) -> dict:
