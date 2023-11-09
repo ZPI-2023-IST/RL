12f123457550b362b4299713f84441f62425e2f0
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2023-11-10 00:13:09 +0100

    Remove illegal moves

--- rl/algorithms/learning_algorithms.py
+++ rl/algorithms/learning_algorithms.py
@@ -6,6 +6,7 @@ import torch
 
 from rl.algorithms import Algorithm, algorithm_manager, Config, ParameterType
 from rl.algorithms.modules.SimpleNet import SimpleNet
+from torch.nn.functional import softmax
 
 """
 Implementation based on Pytorch tutorial
@@ -53,6 +54,7 @@ class DQN(LearningAlgorithm):
 
     def make_action(self, state: list, actions: list[list]) -> list:
         state = torch.tensor(state, dtype=torch.float32, device=self.device).unsqueeze(0)
+        self.state_m = state
 
         sample = random.random()
         eps_threshold = self.config.eps_end + (self.config.eps_start - self.config.eps_end) * \
@@ -61,9 +63,20 @@ class DQN(LearningAlgorithm):
 
         if sample > eps_threshold:
             with torch.no_grad():
-                action_probs = self.policy_net(state)
-                # TO FIX - accomodate for invalid moves
-                return self.policy_net(state).max(1)[1].view(1, 1)
+                # Reduce dimensionality of model output
+                ml_output = self.policy_net(state)[0]
+
+                # We want to remove illegal moves
+                # To make illegal moves deletion easier we calculate softmax so that all our values are in the range of 0 to 1
+                action_probs = softmax(ml_output, dim=0)
+                mod_action_probs = self._remove_invalid_moves(action_probs, actions)
+
+                chosen_action_index = mod_action_probs.argmax().item()
+                action = actions[chosen_action_index]
+
+                self.action_m = torch.tensor([action], device=self.device, dtype=torch.long)
+
+                return action
         else:
             action = random.sample(actions, 1)
             self.action_m = torch.tensor(action, device=self.device, dtype=torch.long)
@@ -79,7 +92,7 @@ class DQN(LearningAlgorithm):
     @classmethod
     def _get_train_params(cls) -> dict:
         return {"n_observations": (ParameterType.INT.name, None, None, None),
-                "all_moves": (ParameterType.LIST.name, None, None, None),
+                "all_actions": (ParameterType.LIST.name, None, None, None),
                 "eps_start": (ParameterType.FLOAT.name, 0.9, 0, 10),
                 "eps_end": (ParameterType.FLOAT.name, 0.05, 0, 10),
                 "eps_decay": (ParameterType.FLOAT.name, 1000, 0, 10000),
@@ -103,13 +116,22 @@ class DQN(LearningAlgorithm):
         #random.seed(self.config.seed)
         #torch.manual_seed(self.config.seed)
         self.memory = ReplayMemory(self.config.memory_size, self.config.batch_size)
-        self.policy_net = SimpleNet([self.config.n_observations, 20, len(self.config.all_moves)]).to(self.device)
-        self.target_net = SimpleNet([self.config.n_observations, 20, len(self.config.all_moves)]).to(self.device)
+        self.policy_net = SimpleNet([self.config.n_observations, 20, len(self.config.all_actions)]).to(self.device)
+        self.target_net = SimpleNet([self.config.n_observations, 20, len(self.config.all_actions)]).to(self.device)
         self.target_net.load_state_dict(self.policy_net.state_dict())
 
         # Things to store later in memory
         self.state_m = None
         self.action_m = None
+
+    # Remove invalid moves by setting the probs of performing given move at -1
+    def _remove_invalid_moves(self, action_probs, actions):
+        for i, action in enumerate(self.config.all_actions):
+            if action not in actions:
+                action_probs[i] = -1
+
+        return action_probs
+
 
         
 
--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 from rl.algorithms.learning_algorithms import DQN
 from rl.logger.Logger import Logger
 
-model_input = [0, 0, 0, 0, 0, 0, 0, 0]
+model_input = [0, 0, 1, 0, 0, 1, 0, 0]
 all_moves = [[1, 1, 1], [0, 0, 0], [1, 0, 1]]
 moves = [[1, 1, 1], [0, 0, 0]]
 
 dqn = DQN(Logger())
 config = dqn.config.as_dict()
 config["n_observations"] = len(model_input)
-config["all_moves"] = all_moves
+config["all_actions"] = all_moves
 dqn.config_model(config)
 
 dqn.make_action(model_input, moves)
