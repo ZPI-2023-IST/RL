8fd35283d19450d96675e5deb0e795c09ae4038e
Patryk Świat <patrykswiat@gmail.com>
2023-11-30 16:08:48 +0100

    add stats endpoint

--- rl/api/Runner.py
+++ rl/api/Runner.py
@@ -115,7 +115,7 @@ class Runner:
     def run(self) -> None:
         self.start_time = time.time()
         port = self.config["game_port"]
-        self.sio.connect(f"http://api:{port}", wait_timeout=10, namespaces=["/"])
+        self.sio.connect(f"http://localhost:{port}", wait_timeout=10, namespaces=["/"])
         self.sio.emit("make_move", json.dumps({"move": None}), namespace="/")
 
         move = None
