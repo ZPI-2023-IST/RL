6f2151915894514102fc54fb70e0f4dba2e62f4e
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2023-12-03 14:56:24 +0100

    Fix error

--- rl/algorithms/learning_algorithms.py
+++ rl/algorithms/learning_algorithms.py
@@ -63,7 +63,6 @@ class DQN(Algorithm):
         self.state_m = torch.tensor(
             state, dtype=torch.float32, device=self.device
         ).flatten().unsqueeze(0)
-        print(self.state_m)
 
         sample = random.random()
         eps_threshold = self.config.eps_end + (
@@ -108,7 +107,7 @@ class DQN(Algorithm):
         # self.state_m contains previous state
         if self.state_m is not None and self.action_m is not None:
             next_state = (
-                torch.tensor([state], dtype=torch.float32).flatten()
+                torch.tensor([state], dtype=torch.float32).flatten().unsqueeze(0)
                 if state is not None
                 else None
             )
