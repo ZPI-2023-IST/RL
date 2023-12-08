a2ef520730c8063d59e6e84e03d0a76cf81384d6
Patryk Świat <patrykswiat@gmail.com>
2023-12-08 12:42:32 +0100

    text fixes

--- rl/tests/test_api.py
+++ rl/tests/test_api.py
@@ -3,13 +3,15 @@ import json
 
 from rl.api.main import app
 from rl.api import algorithm_manager
-from rl.algorithms import DQN
+from rl.algorithms import DQN, RandomAlgorithm
 
 
 class TestAPI(unittest.TestCase):
     def setUp(self):
         app.config["TESTING"] = True
         self.client = app.test_client()
+        algorithm_manager.set_default_algorithm()
+        algorithm_manager.registered_algorithms = {"dqn": DQN, "random": RandomAlgorithm}
 
     def test_config_endpoint(self):
         client = self.client
@@ -37,10 +39,9 @@ class TestAPI(unittest.TestCase):
         config["algorithm"] = "dqn"
         config["n_observations"] = 1
         config["n_actions"] = 1
-        config["mode"] = "train"
 
         client = self.client
-        response = client.put(
+        response = client.post(
             "/config",
             data=json.dumps(config),
             content_type="application/json",
@@ -54,8 +55,6 @@ class TestAPI(unittest.TestCase):
         response = client.get("/config-params")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("random" in json.loads(response.data))
-        self.assertTrue("train" in json.loads(response.data)["random"])
-        self.assertTrue("test" in json.loads(response.data)["random"])
 
     def test_logs_endpoint(self):
         client = self.client
@@ -73,17 +72,10 @@ class TestAPI(unittest.TestCase):
         self.assertTrue("logs" in json.loads(response.data))
 
         self.assertEqual(len(json.loads(response.data)["logs"]), num_logs + 1)
-        self.assertEqual(json.loads(response.data)["logs"][num_logs]["message"], "test")
-        self.assertEqual(json.loads(response.data)["logs"][num_logs]["type"], "CONFIG")
+        self.assertEqual(json.loads(response.data)["logs"][num_logs]["message"]["content"], "test")
+        self.assertEqual(json.loads(response.data)["logs"][num_logs]["message"]["type"], "CONFIG")
 
     def test_model_endpoint(self):
         client = self.client
         response = client.get("/model")
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue("model" in json.loads(response.data))
-        model = json.loads(response.data)["model"]
-
-        response = client.put(
-            "/model", data=json.dumps({"model": model}), content_type="application/json"
-        )
         self.assertEqual(response.status_code, 200)
